ac2ef3fe318b5fcaf1bde8d4c48e5b149180541d
winy <62827232+winyamat@users.noreply.github.com>
2021-09-26 14:04:06 +1300

    Initial commit

---
+++ Data/AppDbcontext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication10.Pages;
+
+namespace WebApplication10.Data
+{
+    public class AppDbcontext:DbContext
+    {
+        public AppDbcontext(DbContextOptions<AppDbcontext> options) : base(options)
+        {
+        }
+           
+           public DbSet<Results> BuzzTable { get; set; }
+        
+    }
+}
---
+++ Data/Results.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication10.Data
+{
+    public class Results
+    {
+        [Key]
+        public int id { get; set; }
+
+        public char results { get; set; }
+
+
+
+
+    }
+}
